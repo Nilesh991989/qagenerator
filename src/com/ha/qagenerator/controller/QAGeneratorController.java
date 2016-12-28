cdd9126270eab9d122e10541cfc86074a1151fc3
nilesh991989 <nileshdoshi991989@gmail.com>
2016-12-28 08:27:00 -1200

    Vcard -> Adding resource to API

--- src/com/ha/qagenerator/controller/QAGeneratorController.java
+++ src/com/ha/qagenerator/controller/QAGeneratorController.java
@@ -1,9 +1,15 @@
 package com.ha.qagenerator.controller;
 
+import org.apache.coyote.http11.Http11AprProtocol;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ha.qagenerator.model.req.VcardReqModel;
 import com.ha.qagenerator.util.QRGenerator;
 
 @RestController
@@ -12,9 +18,9 @@ public class QAGeneratorController {
 	@Autowired
 	QRGenerator qqrGenerator;
 	
-	@RequestMapping(value="generateqr/vcard")
-	public void generateQRCodeVcard(){
-		
+	@RequestMapping(value="generateqr/vcard",method=RequestMethod.GET,produces=MediaType.APPLICATION_JSON_VALUE)
+	public void generateQRCodeVcard(@RequestBody VcardReqModel reqModel){
+		System.out.println(reqModel);
 	}
 	
 	
--- src/com/ha/qagenerator/model/req/VcardReqModel.java
+++ src/com/ha/qagenerator/model/req/VcardReqModel.java
@@ -1,5 +1,32 @@
 package com.ha.qagenerator.model.req;
 
-public class VcardReqModel {
 
+public class VcardReqModel {
+	private String name;	
+	private String mnumber;
+	private String addr;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getMnumber() {
+		return mnumber;
+	}
+	
+	public void setMnumber(String mnumber) {
+		this.mnumber = mnumber;
+	}
+	
+	public String getAddr() {
+		return addr;
+	}
+	
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
 }
